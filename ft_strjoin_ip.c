ede59b5008f02e28891b31661d78e50722a5ad57
Victor Luc Etienne Viterbo <vviterbo@c1r16s7.42lausanne.ch>
2024-11-04 13:52:41 +0100

    improved strjoin_ip

--- ft_strjoin_ip.c
+++ ft_strjoin_ip.c
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 20:14:52 by vviterbo          #+#    #+#             */
-/*   Updated: 2024/10/28 20:19:28 by vviterbo         ###   ########.fr       */
+/*   Updated: 2024/11/04 13:52:17 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,14 @@ char	*ft_strjoin_ip(char const *s1, char const *s2, int in_place)
 	char	*joined;
 
 	joined = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
-	if (!joined && in_place)
+	if (!joined || !s1 || !s2)
 	{
-		if (in_place == 1 || in_place == 3)
+		if (s1 && (in_place == 1 || in_place == 3))
 			free((void *)s1);
-		else if (in_place == 2 || in_place == 3)
+		if (s2 && (in_place == 2 || in_place == 3))
 			free((void *)s2);
+		if (joined)
+			free(joined);
 		return (NULL);
 	}
 	ft_memmove(joined, s1, ft_strlen(s1));
@@ -32,7 +34,7 @@ char	*ft_strjoin_ip(char const *s1, char const *s2, int in_place)
 	*(joined + ft_strlen(s1) + ft_strlen(s2)) = '\0';
 	if (in_place == 1 || in_place == 3)
 		free((void *)s1);
-	else if (in_place == 2 || in_place == 3)
+	if (in_place == 2 || in_place == 3)
 		free((void *)s2);
 	return (joined);
 }
