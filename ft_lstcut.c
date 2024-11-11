2ee0463ea7b664e5510bef70d141197f36031b94
vviterbo <victor.viterbo@epfl.ch>
2024-11-11 20:18:35 +0100

    updated lstcut

--- ft_lstcut.c
+++ ft_lstcut.c
@@ -6,15 +6,15 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 20:01:49 by vviterbo          #+#    #+#             */
-/*   Updated: 2024/11/11 20:12:58 by vviterbo         ###   ########.fr       */
+/*   Updated: 2024/11/11 20:16:23 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	**ft_lstcut(t_list **out, int start, int end);
+t_list	**ft_lstcut(t_list **lst, void (*del)(void *), int start, int end);
 
-t_list	**ft_lstcut(t_list **lst, int start, int end)
+t_list	**ft_lstcut(t_list **lst, void (*del)(void *), int start, int end)
 {
 	t_list	*current;
 	t_list	*last;
@@ -29,7 +29,7 @@ t_list	**ft_lstcut(t_list **lst, int start, int end)
 		if (i == start - 1)
 			last = current;
 		else if (start <= i && i < end)
-			ft_lstdelone(current, free);
+			ft_lstdelone(current, del);
 		else if (i == end && start)
 			last->next = current;
 		else if (i == end)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 12:33:18 by vviterbo          #+#    #+#             */
-/*   Updated: 2024/11/11 20:03:37 by vviterbo         ###   ########.fr       */
+/*   Updated: 2024/11/11 20:17:02 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,7 +100,7 @@ char	**ft_strarray_mapi(char **strarray, char *(*f)(const char *));
 size_t	ft_arrlen(char **array);
 void	ft_free_array(void **array, int size);
 double	ft_parse_as(void *ptr, int dtype);
-t_list	**ft_lstcut(t_list **out, int start, int end);
+t_list	**ft_lstcut(t_list **lst, void (*del)(void *), int start, int end);
 
 char	*ft_get_next_line(int fd);
 char	*ft_strjoin_gnl(char const *s1, char const *s2, int in_place);
