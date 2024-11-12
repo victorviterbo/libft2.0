9e9a2c5c217ed23031286e48e9aa5eac36fb6361
vviterbo <victor.viterbo@epfl.ch>
2024-11-12 13:51:27 +0100

    corrected slight norm error

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 10:20:37 by vviterbo          #+#    #+#             */
-/*   Updated: 2024/11/12 13:36:12 by vviterbo         ###   ########.fr       */
+/*   Updated: 2024/11/12 13:51:04 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new);
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-
 	if (!lst)
 		return ;
 	if (!(*lst))
