c09ae01347f1e0c1cba5af7215d322c72c0b936d
vviterbo <victor.viterbo@epfl.ch>
2024-11-12 09:44:38 +0100

    ongoing

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 10:20:37 by vviterbo          #+#    #+#             */
-/*   Updated: 2024/10/11 12:02:30 by vviterbo         ###   ########.fr       */
+/*   Updated: 2024/11/12 09:40:07 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,8 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	if (!lst)
 		return ;
 	if (!(*lst))
-	{
 		*lst = new;
-		return ;
-	}
-	last = ft_lstlast(*lst);
-	last->next = new;
+	else
+		ft_lstlast(*lst)->next = new;
 	return ;
 }
--- ft_lstcut.c
+++ ft_lstcut.c
@@ -6,15 +6,15 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 20:01:49 by vviterbo          #+#    #+#             */
-/*   Updated: 2024/11/11 20:16:23 by vviterbo         ###   ########.fr       */
+/*   Updated: 2024/11/12 08:00:02 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	**ft_lstcut(t_list **lst, void (*del)(void *), int start, int end);
+void	ft_lstcut(t_list **lst, void (*del)(void *), int start, int end);
 
-t_list	**ft_lstcut(t_list **lst, void (*del)(void *), int start, int end)
+void	ft_lstcut(t_list **lst, void (*del)(void *), int start, int end)
 {
 	t_list	*current;
 	t_list	*last;
@@ -22,10 +22,13 @@ t_list	**ft_lstcut(t_list **lst, void (*del)(void *), int start, int end)
 	int		i;
 
 	i = 0;
+	if (!lst)
+		return ;
 	current = *lst;
-	while (current)
+	while (current && current->next)
 	{
-		next = current->next;
+		printf("LALA\n");
+		printf("i = %i\n", i);
 		if (i == start - 1)
 			last = current;
 		else if (start <= i && i < end)
@@ -34,10 +37,23 @@ t_list	**ft_lstcut(t_list **lst, void (*del)(void *), int start, int end)
 			last->next = current;
 		else if (i == end)
 			*lst = current;
-		current = next;
+		printf("i 2 = %i\n", i);
+		printf("%i\n", i);
+		printf("%p\n", current);
 		i++;
+		printf("%p\n", current);
+		current = next;
 	}
+	if (i == start - 1)
+		last = current;
+	else if (start <= i && i < end)
+		ft_lstdelone(current, del);
+	else if (i == end && start)
+		last->next = current;
+	else if (i == end)
+		*lst = current;
+	i++;
 	if (i <= end)
 		last->next = NULL;
-	return (lst);
+	return ;
 }
