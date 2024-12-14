7970b0b2c30f241eb9477a94bea6b793b0d7cc73
vviterbo <victor.viterbo@epfl.ch>
2024-12-14 18:00:36 +0100

    change var name, was complaining

--- lists/ft_lstadd_back.c
+++ lists/ft_lstadd_back.c
@@ -6,21 +6,21 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 10:20:37 by vviterbo          #+#    #+#             */
-/*   Updated: 2024/12/10 10:19:29 by vviterbo         ###   ########.fr       */
+/*   Updated: 2024/12/14 17:59:47 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new_node);
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new_node)
 {
 	if (!lst)
 		return ;
 	if (!(*lst))
-		*lst = new;
+		*lst = new_node;
 	else
-		ft_lstlast(*lst)->next = new;
+		ft_lstlast(*lst)->next = new_node;
 	return ;
 }
--- lists/ft_lstadd_front.c
+++ lists/ft_lstadd_front.c
@@ -6,19 +6,19 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 10:00:42 by vviterbo          #+#    #+#             */
-/*   Updated: 2024/12/10 10:19:29 by vviterbo         ###   ########.fr       */
+/*   Updated: 2024/12/14 17:59:57 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new_node);
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new_node)
 {
 	if (!lst)
 		return ;
-	new->next = *lst;
-	*lst = new;
+	new_node->next = *lst;
+	*lst = new_node;
 	return ;
 }
