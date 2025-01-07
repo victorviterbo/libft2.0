294fcee60d5091677b4f506978b3340b791f8cbd
Victor Luc Etienne Viterbo <vviterbo@c1r16s7.42lausanne.ch>
2025-01-07 20:29:25 +0100

    improved ft_array_apppend

--- miscs/ft_array_append.c
+++ miscs/ft_array_append.c
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 12:35:52 by vviterbo          #+#    #+#             */
-/*   Updated: 2025/01/04 12:36:23 by vviterbo         ###   ########.fr       */
+/*   Updated: 2025/01/07 20:29:05 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	**ft_array_append(char **array, char *str, bool first)
 	size_t	i;
 	char	**concatenated;
 
+	if (!str)
+		return (array);
 	concatenated = ft_calloc(ft_arrlen(array) + 2, sizeof(char *));
 	i = 0;
 	if (first)
