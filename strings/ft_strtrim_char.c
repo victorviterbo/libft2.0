b1b425b91cbfb494ae810c78d0b1bf812e67a4a2
vviterbo <victor.viterbo@epfl.ch>
2025-02-20 14:07:52 +0100

    finished implementing tree

--- strings/ft_strtrim_char.c
+++ strings/ft_strtrim_char.c
@@ -6,15 +6,15 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 23:22:58 by vviterbo          #+#    #+#             */
-/*   Updated: 2025/02/14 17:59:06 by vviterbo         ###   ########.fr       */
+/*   Updated: 2025/02/20 13:55:53 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim_char(char	*str, char c, bool inplace);
+char	*ft_strtrim_char(char *str, char c, bool inplace);
 
-char	*ft_strtrim_char(char	*str, char c, bool inplace)
+char	*ft_strtrim_char(char *str, char c, bool inplace)
 {
 	char	*cstr;
 	char	*trimmed;
